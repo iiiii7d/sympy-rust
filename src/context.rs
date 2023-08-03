9f0d00a36b75df7126d556ed29a802538e5be109
7d <61975820+iiiii7d@users.noreply.github.com>
2023-08-03 09:11:27 +0800

    split into Symbol and SymbolGIL

--- src/context.rs
+++ src/context.rs
@@ -1,5 +1,5 @@
 use pyo3::prelude::*;
-use crate::symbol::Symbol;
+use crate::symbol::{Symbol, SymbolGIL};
 
 #[derive(Clone, Debug)]
 pub struct Context {
@@ -15,10 +15,16 @@ impl Context {
         })
     }
     pub fn symbol<T: ToString>(&self, name: T) -> PyResult<Symbol> {
-        Symbol::new(self, name)
+        Python::with_gil(|py| self.symbol_gil(py, name).map(|a| a.un_gil()))
+    }
+    pub fn symbol_gil<'py, 'a: 'py, T: ToString>(&'a self, py: Python<'py>, name: T) -> PyResult<SymbolGIL<'py>> {
+        SymbolGIL::new(py, self, name)
     }
     pub fn symbol_non_commutative<T: ToString>(&self, name: T) -> PyResult<Symbol> {
-        Symbol::new_non_commutative(self, name)
+        Python::with_gil(|py| self.symbol_non_commutative_gil(py, name).map(|a| a.un_gil()))
+    }
+    pub fn symbol_non_commutative_gil<'py, 'a: 'py, T: ToString>(&'a self, py: Python<'py>, name: T) -> PyResult<SymbolGIL<'py>> {
+        SymbolGIL::new(py, self, name)
     }
     pub(crate) fn sympy<'py, 'a: 'py>(&'a self, py: Python<'py>) -> &'py PyModule {
         self.sympy.as_ref(py)
--- src/symbol.rs
+++ src/symbol.rs
@@ -3,42 +3,40 @@ use crate::context::Context;
 use crate::py_dict;
 
 #[derive(Clone, Debug)]
-pub struct Symbol(Py<PyAny>);
-impl Symbol {
-    pub fn class<'py, 'a: 'py>(py: Python<'py>, ctx: &'a Context) -> PyResult<&'py PyAny> {
+pub struct SymbolGIL<'py>(&'py PyAny);
+impl<'py> SymbolGIL<'py> {
+    pub fn class<'a: 'py>(py: Python<'py>, ctx: &'a Context) -> PyResult<&'py PyAny> {
         ctx.sympy(py).getattr("Symbol")
     }
-    pub fn new_gil<T: ToString>(py: Python, ctx: &Context, name: T) -> PyResult<Self> {
-        Ok(Self(Self::class(py, ctx)?.call1((name.to_string(), ))?.into()))
+    pub fn un_gil(&self) -> Symbol {
+        Symbol(self.0.into())
     }
-    pub fn new_non_commutative_gil<T: ToString>(py: Python, ctx: &Context, name: T) -> PyResult<Self> {
-        Ok(Self(Self::class(py, ctx)?.call((name.to_string(), ), Some(py_dict! {py, "commutative" => false}))?.into()))
+    pub fn new<'a: 'py, T: ToString>(py: Python<'py>, ctx: &'a Context, name: T) -> PyResult<Self> {
+        let res = Self::class(py, ctx)?.call1((name.to_string(), ))?;
+        Ok(Self(res))
     }
-    pub fn name_gil(&self, py: Python) -> PyResult<String> {
-        self.0.as_ref(py).getattr("name")?.extract::<String>()
+    pub fn new_non_commutative<'a: 'py, T: ToString>(py: Python<'py>, ctx: &'a Context, name: T) -> PyResult<Self> {
+        let res = Self::class(py, ctx)?.call((name.to_string(), ), Some(py_dict! {py, "commutative" => false}))?;
+        Ok(Self(res))
     }
-    pub fn set_name_gil<T: ToString>(&self, py: Python, name: T) -> PyResult<()> {
-        self.0.as_ref(py).setattr("name", name.to_string())
-    }
-    pub fn new<T: ToString>(ctx: &Context, name: T) -> PyResult<Self> {
-        Python::with_gil(|py| {
-            Self::new_gil(py, ctx, name)
-        })
-    } // TODO derive
-
-    pub fn new_non_commutative<T: ToString>(ctx: &Context, name: T) -> PyResult<Self> {
-        Python::with_gil(|py| {
-            Self::new_non_commutative_gil(py, ctx, name)
-        })
-    } // TODO derive
     pub fn name(&self) -> PyResult<String> {
-        Python::with_gil(|py| {
-            self.name_gil(py)
-        })
+        self.0.getattr("name")?.extract::<String>()
     }
     pub fn set_name<T: ToString>(&self, name: T) -> PyResult<()> {
-        Python::with_gil(|py| {
-            self.set_name_gil(py, name)
-        })
+        self.0.setattr("name", name.to_string())
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Symbol(Py<PyAny>);
+impl Symbol {
+    pub fn gil<'py, 'a: 'py>(&'a self, py: Python<'py>) -> SymbolGIL<'py> {
+        SymbolGIL(self.0.as_ref(py))
+    }
+    pub fn name(&self) -> PyResult<String> {
+        Python::with_gil(|py| self.gil(py).name())
+    }
+    pub fn set_name<T: ToString>(&self, name: T) -> PyResult<()> {
+        Python::with_gil(|py| self.gil(py).set_name(name))
     }
 }
