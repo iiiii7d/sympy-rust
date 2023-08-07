ae9170b0542438a22c3682be3b0a67165ba515d5
7d <61975820+iiiii7d@users.noreply.github.com>
2023-08-07 12:29:27 +0800

    remove 'static req for set_name

--- src/context.rs
+++ src/context.rs
@@ -18,13 +18,15 @@ impl<'py> Context<'py> {
             sympy: py.import("sympy")?.into(),
         })
     }
-    pub fn with_gil<R, F: FnOnce(Context) -> R>(f: F) -> PyResult<R> {
+    pub fn with_gil<'b, R, F: for<'a> FnOnce(Context<'a>) -> R + 'b>(f: F) -> PyResult<R> {
         Python::with_gil(|py| {
             let ctx = Context::new(py)?;
             Ok(f(ctx))
         })
     }
-    pub fn try_with_gil<R, F: FnOnce(Context) -> PyResult<R> + 'static>(f: F) -> PyResult<R> {
+    pub fn try_with_gil<'b, R, F: for<'a> FnOnce(Context<'a>) -> PyResult<R> + 'b>(
+        f: F,
+    ) -> PyResult<R> {
         Python::with_gil(|py| {
             let ctx = Context::new(py)?;
             f(ctx)
--- src/core/symbol/symbol.rs
+++ src/core/symbol/symbol.rs
@@ -11,18 +11,21 @@ use crate::{
 
 pub trait SymbolImpl {
     fn name(&self) -> PyResult<String>;
-    fn set_name<T: ToString + 'static>(&self, name: T) -> PyResult<()>;
+    fn set_name<T: ToString + ?Sized>(&self, name: &T) -> PyResult<()>;
 }
 
 #[derive(Clone, Debug, Object)]
 #[object(class_name = "Symbol")]
 pub struct Symbol(PyObject);
 impl<'py, 'a, 'b> GIL<'py, 'a, 'b, Symbol> {
-    pub fn new<T: ToString>(ctx: &'a Context<'py>, name: T) -> PyResult<Self> {
+    pub fn new<T: ToString + ?Sized>(ctx: &'a Context<'py>, name: &T) -> PyResult<Self> {
         let res = Self::class(ctx)?.call1((name.to_string(),))?;
         Ok(Self(Cow::Owned(Symbol(res.into())), ctx))
     }
-    pub fn new_non_commutative<T: ToString>(ctx: &'a Context<'py>, name: T) -> PyResult<Self> {
+    pub fn new_non_commutative<T: ToString + ?Sized>(
+        ctx: &'a Context<'py>,
+        name: &T,
+    ) -> PyResult<Self> {
         let res = Self::class(ctx)?.call(
             (name.to_string(),),
             Some(py_dict! {ctx.gil, "commutative" => false}),
@@ -35,16 +38,16 @@ impl<'py, 'a, 'b> SymbolImpl for GIL<'py, 'a, 'b, Symbol> {
     fn name(&self) -> PyResult<String> {
         self.py_inner().getattr("name")?.extract::<String>()
     }
-    fn set_name<T: ToString + 'static>(&self, name: T) -> PyResult<()> {
+    fn set_name<T: ToString + ?Sized>(&self, name: &T) -> PyResult<()> {
         self.py_inner().setattr("name", name.to_string())
     }
 }
 
 impl<'py> Context<'py> {
-    pub fn symbol<T: ToString>(&self, name: T) -> PyResult<GIL<Symbol>> {
+    pub fn symbol<T: ToString + ?Sized>(&self, name: &T) -> PyResult<GIL<Symbol>> {
         GIL::<Symbol>::new(self, name)
     }
-    pub fn symbol_non_commutative<T: ToString>(&self, name: T) -> PyResult<GIL<Symbol>> {
+    pub fn symbol_non_commutative<T: ToString + ?Sized>(&self, name: &T) -> PyResult<GIL<Symbol>> {
         GIL::<Symbol>::new_non_commutative(self, name)
     }
 }
